ac2af5286c2a8fa8096eccde458a15b2b946b32a
Rishav Thakker <thakker.rishav@gmail.com>
2022-05-07 16:51:04 +0530

    Organized code

--- drive.py
+++ drive.py
@@ -6,18 +6,22 @@ import json
 from pydrive2.drive import GoogleDrive
 from pydrive2.auth import GoogleAuth
 
+SA_JSON_PATH = "sa.json"
+DRIVE_JSON_PATH = "drive.json"
+
+
 def get_pydrive2_service():
     """Returns the pydrive2.drive.GoogleDrive object"""
     auth = GoogleAuth()
-    sa_json_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-    with open(sa_json_path, encoding="utf-8") as file:
+    with open(SA_JSON_PATH, encoding="utf-8") as file:
         sa_json = json.load(file)
     auth.settings["service_config"] = {
-        "client_json_file_path": sa_json_path,
+        "client_json_file_path": SA_JSON_PATH,
         "client_user_email": sa_json["client_email"],
     }
     auth.ServiceAuth()
     return GoogleDrive(auth)
+
 
 def upload(local_path, pyd_service):
     """Uploads a file and returns the file ID"""
@@ -26,26 +30,26 @@ def upload(local_path, pyd_service):
     file.Upload()
     return file["id"]
 
+
 def add_parent(file_id, parent_id, pyd_service):
     """Adds a parent folder to the given file ID"""
     request = pyd_service.auth.service.parents().insert(
-        fileId=file_id,
-        body={
-            "id": parent_id
-        }
+        fileId=file_id, body={"id": parent_id}
     )
     return request.execute()
+
 
 def main():
     """Uploads the given file (1st arg in cmd) to the root folder and prints
     the file id"""
     filepath = sys.argv[1]
-    with open("drive.json", encoding="utf-8") as file:
+    with open(DRIVE_JSON_PATH, encoding="utf-8") as file:
         folder_id = json.load(file)["root_folder_id"]
     pyd_service = get_pydrive2_service()
     file_id = upload(filepath, pyd_service)
     assert add_parent(file_id, folder_id, pyd_service)
     print(f"Upload {file_id} to {folder_id}")
+
 
 if __name__ == "__main__":
     main()
