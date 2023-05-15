d4e3e944a20dd877e4b44c39ef99bc396309d455
Rishav Thakker <thakker.rishav@gmail.com>
2023-05-15 09:38:03 +0530

    added init and set name

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name="sampleproject",  # Required
+    name="wautomark",  # Required
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
     #
